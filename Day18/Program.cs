7ebd209c43acdbc31ed54d881517073d5f4f43ce
scalonne <calonne.sebastien+github@gmail.com>
2025-01-07 13:17:44 +0100

    day18 optimizations
    from 1s to 50ms
    - bisect search

--- Day18/Program.cs
+++ Day18/Program.cs
@@ -1,13 +1,13 @@
 ﻿var Bytes = File.ReadLines("input.txt")
                 .Select(o => o.Split(','))
-                .Select(o => (x: Int32.Parse(o[0]), y: Int32.Parse(o[1])))
+                .Select(o => new Pos(Int32.Parse(o[0]), Int32.Parse(o[1])))
                 .ToList();
 const int MapSize = 70;
 const int Width = MapSize + 1;
 const int Height = MapSize + 1;
 const int FeltBytes = 1_024;
 
-var End = (Width - 1, Height - 1);
+var End = new Pos(Width - 1, Height - 1);
 var Directions = new (int dx, int dy)[] {
     (0, 1),
     (0, -1),
@@ -15,21 +15,20 @@ var Directions = new (int dx, int dy)[] {
     (-1 , 0)
 };
 
-List<List<(int, int)>> Escape(int feltBytes) {
+List<List<Pos>> Escape(int feltBytes) {
     int? steps = null;
-    var bytes = new HashSet<(int, int)>(Bytes[..feltBytes]);
-    var paths = new List<List<(int, int)>>();
-    var visitedSet = new HashSet<(int, int)>();
-    var visitedStack = new Stack<(int, int)>();
-    var shortestByCell = new Dictionary<(int, int), int>();
+    var bytes = new HashSet<Pos>(Bytes[..feltBytes]);
+    var paths = new List<List<Pos>>();
+    var visitedSet = new HashSet<Pos>();
+    var shortestByCell = new Dictionary<Pos, int>();
 
-    escapeRec((0, 0), 0);
+    escapeRec(new(0, 0), 0);
 
     return paths;
 
-    void escapeRec((int x, int y) pos, int currentSteps) {
+    void escapeRec(Pos pos, int currentSteps) {
         if (pos == End) {
-            paths.Add(new(visitedStack));
+            paths.Add(visitedSet.ToList());
 
             if (steps == null || currentSteps < steps) {
                 steps = currentSteps;
@@ -38,19 +37,15 @@ List<List<(int, int)>> Escape(int feltBytes) {
             return;
         }
 
-        // disabled for p2
-        //if (currentSteps == steps)
-        //    return;
-
-        if (shortestByCell.TryGetValue(pos, out var cellShortest) && cellShortest <= currentSteps)
+        if (currentSteps == steps || shortestByCell.TryGetValue(pos, out var cellShortest) && cellShortest <= currentSteps)
             return;
 
         shortestByCell[pos] = currentSteps;
 
         foreach ((var dx, var dy) in Directions) {
-            var x = pos.x + dx;
-            var y = pos.y + dy;
-            var next = (x, y);
+            var x = pos.X + dx;
+            var y = pos.Y + dy;
+            var next = new Pos(x, y);
 
             if (visitedSet.Contains(next)
                 || bytes.Contains(next)
@@ -58,36 +53,36 @@ List<List<(int, int)>> Escape(int feltBytes) {
                 continue;
 
             visitedSet.Add(next);
-            visitedStack.Push(next);
             escapeRec(next, currentSteps + 1);
             visitedSet.Remove(next);
-            visitedStack.Pop();
         }
     }
 }
 
-int P1(out List<List<(int, int)>> paths) {
-    paths = Escape(FeltBytes);
+int P1()
+    => Escape(FeltBytes).Min(o => o.Count);
 
-    return paths.Min(o => o.Count);
-}
+string P2() {
+    var i = (Bytes.Count - FeltBytes) / 2;
+    var start = FeltBytes + i;
+    var paths = Escape(start);
 
-string P2(List<List<(int, int)>> paths) {
-    foreach (var i in Enumerable.Range(FeltBytes, Bytes.Count - FeltBytes)) {
-        var fallingByte = Bytes[i - 1];
+    while (i > 1) {
+        start += paths.Count == 0 ? -i : i;
 
-        paths.RemoveAll(o => o.Contains(fallingByte));
+        paths = Escape(start);
 
-        if (paths.Count == 0) {
-            paths = Escape(i);
-
-            if (paths.Count == 0)
-                return $"{fallingByte.x},{fallingByte.y}";
-        }
+        i /= 2;
     }
 
-    throw new InvalidOperationException();
+    while (paths.Count == 0) {
+        paths = Escape(--start);
+    }
+
+    return $"{Bytes[start].X},{Bytes[start].Y}";
 }
 
-Console.WriteLine(P1(out var path));
-Console.WriteLine(P2(path));
+Console.WriteLine(P1());
+Console.WriteLine(P2()); 
+
+record struct Pos(int X, int Y);
