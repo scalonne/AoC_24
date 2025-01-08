783369f11f2ba1aa79f4b34c32f0ef01c0203e29
scalonne <calonne.sebastien+github@gmail.com>
2025-01-08 13:27:16 +0100

    day06 simplified and bit faster but not quite yet optimized
    missing teleportation instead of incrementing step by step

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -1,153 +1,113 @@
-﻿var lines = File.ReadAllLines("input.txt")
-                .Select((l, y) => l.Select((c, x) => (c, x, y))
-                                   .ToList())
-                .ToList();
-var Height = lines.Count;
-var Width = lines[0].Count;
+var Map = File.ReadLines("input.txt")
+               .Select((o, y) => o.Select((o, x) => new Cell(new Pos(x, y), o))
+                                  .ToArray())
+               .ToArray();
+var CellByPos = Map.SelectMany(o => o)
+                   .ToDictionary(o => o.P, o => o.C);
+var Height = Map.Length;
+var Width = Map[0].Length;
+var Directions = new Direction[] {
+    new(0, -1), // N 
+    new(1, 0),  // E
+    new(0, 1),  // S
+    new(-1, 0), // W
+};
 
-var start = lines.SelectMany(o => o)
-                 .First(o => o.c == '^');
-
-var walls = lines.SelectMany(o => o)
-                 .Where(o => o.c == '#')
-                 .Select(o => (o.x, o.y));
-
-var xWalls = new Dictionary<int, (List<int> yAsc, List<int> yDesc)>();
-var yWalls = new Dictionary<int, (List<int> xAsc, List<int> xDesc)>();
-
-void Init() {
-    foreach ((var x, var y) in walls) {
-        AddWall(x, y);
-    }
-}
-
-void AddWall(int x, int y) {
-    if (!xWalls.ContainsKey(x))
-        xWalls[x] = (new List<int>(), new List<int>());
-
-    if (!yWalls.ContainsKey(y))
-        yWalls[y] = (new List<int>(), new List<int>());
-
-    void insert(List<int> l, int v, bool asc = true) {
-        var i = 0;
-        while (i < l.Count) {
-            if (asc && l[i] > v || !asc && l[i] < v)
-                break;
-            i++;
-        }
-        l.Insert(i, v);
-    }
-
-    insert(xWalls[x].yAsc, y);
-    insert(xWalls[x].yDesc, y, false);
-    insert(yWalls[y].xAsc, x);
-    insert(yWalls[y].xDesc, x, false);
-}
-
-void RemoveWall(int x, int y) {
-    xWalls[x].yAsc.Remove(y);
-
-    if (xWalls[x].yAsc.Count == 0)
-        xWalls.Remove(x);
-    else
-        xWalls[x].yDesc.Remove(y);
-
-    yWalls[y].xAsc.Remove(x);
-
-    if (yWalls[y].xAsc.Count == 0)
-        yWalls.Remove(y);
-    else
-        yWalls[y].xDesc.Remove(x);
-}
-
-(bool isLooping, List<Position>? path) TryEscape(Position start, List<Position>? existingPath = null) {
-    var path = new List<Position>(existingPath ?? []);
-    var visited = new HashSet<Position>(path);
-    (var x, var y, var direction) = start;
-    var isLooping = false;
+List<(Pos, Direction dir)>? Travel(List<(Pos pos, Direction dir)> path, bool fastForward) {
+    var visited = new HashSet<(Pos pos, Direction dir)>(path);
+    (var pos, var d) = path.Last();
+    var i = Directions.Index().First(o => o.Item == d).Index;
 
     while (true) {
-        void updateVisited(int from, int to, Func<int, (int x, int y)> getPos) {
-            var j = from < to ? 1 : -1;
+        var x = pos.X + d.Dx;
+        var y = pos.Y + d.Dy;
 
-            to += j;
+        if (!IsInbound(x, y))
+            break;
 
-            for (var i = from; i != to; i += j) {
-                var pos = getPos(i);
-                var node = new Position(pos.x, pos.y, direction);
+        if (fastForward && Map[y][x].C != '#') {
+            x += d.Dx;
+            y += d.Dy;
 
-                if (!visited.Add(node)) {
-                    isLooping = true;
-                    return;
-                }
+            while (Map[y][x].C != '#') {
+                x += d.Dx;
+                y += d.Dy;
 
-                path.Add(node);
+                if (!IsInbound(x, y))
+                    return path;
             }
+
+            pos = new Pos(x - d.Dx, y - d.Dy);
         }
 
-        int? newX = x;
-        int? newY = y;
+        var next = new Pos(pos.X + d.Dx, pos.Y + d.Dy);
 
-        if (direction == Direction.North) {
-            newY = (!xWalls.TryGetValue(x, out var l) || l.yAsc[0] >= y) ? null : l.yDesc.First(o => o < y) + 1;
-            updateVisited(y, newY ?? 0, o => (x, o));
-        } else if (direction == Direction.East) {
-            newX = (!yWalls.TryGetValue(y, out var l) || l.xDesc[0] <= x) ? null : l.xAsc.First(o => o > x) - 1;
-            updateVisited(x, newX ?? (Width - 1), o => (o, y));
-        } else if (direction == Direction.South) {
-            newY = (!xWalls.TryGetValue(x, out var l) || l.yDesc[0] <= y) ? null : l.yAsc.First(o => o > y) - 1;
-            updateVisited(y, newY ?? (Height - 1), o => (x, o));
+        if (CellByPos[next] == '#') {
+            i = i == 3 ? 0 : i + 1;
+            d = Directions[i];
         } else {
-            newX = (!yWalls.TryGetValue(y, out var l) || l.xAsc[0] >= x) ? null : l.xDesc.First(o => o < x) + 1;
-            updateVisited(x, newX ?? 0, o => (o, y));
+            pos = next;
         }
 
-        if (isLooping)
-            return (true, null);
+        var node = (pos, d);
 
-        if (!newX.HasValue || !newY.HasValue)
-            return (false, path);
+        if (!visited.Add(node))
+            return null;
 
-        x = newX!.Value;
-        y = newY!.Value;
-        direction = direction == Direction.West ? Direction.North : direction + 1;
-    }
-}
-
-int P2(List<Position> p1) {
-    var loopCount = 0;
-    var path = new List<Position>();
-    var walls = new HashSet<(int, int)>();
-    var cpy = p1.ToList();
-
-    while (cpy.Count > 1) {
-        var start = cpy[0];
-        (var x, var y, _) = cpy[1];
-
-        cpy.RemoveAt(0);
-
-        if (walls.Add((x, y))) {
-            AddWall(x, y);
-
-            if (TryEscape(start, path).isLooping)
-                loopCount++;
-
-            RemoveWall(x, y);
-        }
-
-        path.Add(start);
+        if (!fastForward)
+            path.Add(node);
     }
 
-    return loopCount;
+    return path;
+
+    bool IsInbound(int x, int y)
+        => x >= 0 && y >= 0 && x < Width && y < Height;
 }
 
-Init();
+List<(Pos pos, Direction dir)> P1() {
+    var start = Map.SelectMany(o => o).First(o => o.C == '^');
 
-var p1 = TryEscape(new(start.x, start.y, Direction.North));
-var p2 = P2(p1.path!);
+    return Travel([(start.P, Directions[0])], false)!;
+}
 
-Console.WriteLine(p1.path!.Select(o => (o.X, o.Y)).Distinct().Count());
+int P2(List<(Pos pos, Direction dir)> path) {
+    var loops = 0;
+    var i = 0;
+    var visited = new HashSet<Pos>();
+
+    path.RemoveAt(0);
+
+    while (i < path.Count - 1) {
+        (var pos, _) = path[i];
+
+        while (path[i].pos == pos)
+            i++;
+
+        (var next, _) = path[i];
+
+        if (!visited.Add(next))
+            continue;
+
+        Map[next.Y][next.X].C = '#';
+        CellByPos[next] = '#';
+
+        if (Travel(path[..i], true) == null)
+            loops++;
+
+        Map[next.Y][next.X].C = '.';
+        CellByPos[next] = '.';
+    }
+
+    return loops;
+}
+
+var path = P1();
+var p1 = path!.Select(o => o.pos).Distinct().Count();
+var p2 = P2(path);
+
+Console.WriteLine(p1);
 Console.WriteLine(p2);
 
-enum Direction { North = 0, East = 1, South = 2, West = 3 }
-file record struct Position(int X, int Y, Direction Direction);
+record struct Direction(int Dx, int Dy);
+record struct Pos(int X, int Y);
+record struct Cell(Pos P, char C);
